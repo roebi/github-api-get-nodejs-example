3511ef0438ae9691762abce507faaba7461a3bcf
roebi <roebi@users.noreply.github.com>
2023-09-20 02:03:25 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# github-api-get-nodejs-example
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/github-api-get-nodejs)
---
+++ index.js
@@ -0,0 +1,19 @@
+export {};
+
+import { getGithubTopics } from 'github-api-get';
+
+async function getGithubTopicsfromgithub() {
+  const loginName = 'roebi';
+  const projectName = '01-01-vanilla-HTML5-starter-page';
+  const infoLog = true;
+  const isProd = true;
+  const githubTopics = getGithubTopics(loginName, projectName, infoLog, isProd); // as Promise<String[]>;
+
+  const realGithubTopics = await githubTopics;
+  console.group('realGithubTopics');
+  console.info('realGithubTopics');
+  console.info(realGithubTopics);
+  console.groupEnd();
+}
+
+await getGithubTopicsfromgithub();
