5b735815ea58e060aa550ce3f391a16da8cdd5f9
roebi <roebi@users.noreply.github.com>
2023-09-20 12:41:29 +0200

    use 3.0.7

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ async function getGithubTopicsfromgithub() {
   const projectName = '01-01-vanilla-HTML5-starter-page';
   const infoLog = true;
   const isProd = true;
-  const githubTopics = getGithubTopics(loginName, projectName, infoLog, isProd); // as Promise<String[]>;
+  const githubTopics = getGithubTopics(loginName, projectName, infoLog, isProd); // as Promise<string[]>;
 
   const realGithubTopics = await githubTopics;
   console.group('realGithubTopics');
