e2b50f7c4c1669c80c46a1d912c3c67a234d1bdf
Robert Halter <roebi@users.noreply.github.com>
2024-08-01 15:52:24 +0200

    Update index.js: use of githubApiGetProject

--- index.js
+++ index.js
@@ -1,19 +1,45 @@
 export {};
 
-import { getGithubTopics } from 'github-api-get';
+import { githubApiGetProject, getGithubTopics } from 'github-api-get';
+import { GITHUB_PROJECT_TOPICS } from './consts.js';
+
+async function realdataTopicsWithPathExpression() {
+  const loginName = 'roebi';
+  const projectName = '01-01-vanilla-HTML5-starter-page';
+  const infoLog = true;
+  const isProd = true;
+  const githubTopics = githubApiGetProject(
+    loginName,
+    projectName,
+    GITHUB_PROJECT_TOPICS,
+    infoLog,
+    isProd
+  ); // as Promise<string[]>;
+
+  const realGithubTopics = await githubTopics;
+  console.group(
+    'realGithubTopics, call of githubApiGetProject with constant GITHUB_PROJECT_TOPICS'
+  );
+  console.info('realGithubTopics');
+  console.info(realGithubTopics);
+  console.groupEnd();
+}
 
 async function getGithubTopicsfromgithub() {
   const loginName = 'roebi';
   const projectName = '01-01-vanilla-HTML5-starter-page';
   const infoLog = true;
   const isProd = true;
+  // this call is deprecated, use githubApiGetProject with constant GITHUB_PROJECT_TOPICS
   const githubTopics = getGithubTopics(loginName, projectName, infoLog, isProd); // as Promise<string[]>;
 
   const realGithubTopics = await githubTopics;
-  console.group('realGithubTopics');
+  console.group('realGithubTopics, deprecated call getGithubTopics');
   console.info('realGithubTopics');
   console.info(realGithubTopics);
   console.groupEnd();
 }
 
+await realdataTopicsWithPathExpression();
+console.info();
 await getGithubTopicsfromgithub();
